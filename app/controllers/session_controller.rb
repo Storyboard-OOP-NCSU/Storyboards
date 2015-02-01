db45f085eed1bb11bb245c43a3b2814bd54d9dc3
Your Name <you@example.com>
2015-02-01 15:38:33 -0500

    add position tag in Log in page to recognize user is admin or developer

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,19 +1,33 @@
 class SessionController < ApplicationController
-  # attr_reader :admin
   def new
   end
 
   def create
-    if admin = Admin.authenticate(params[:name], params[:password])
-      session[:admin_id] = admin.id
-      redirect_to admin
-    else
-      redirect_to login_url
-    end
+ 		session[:position] = params[:position]
+	  if session[:position] == ['Admin']
+ 	  	if admin = Admin.authenticate(params[:name], params[:password])
+  	    session[:admin_id] = admin.id
+ 	    	redirect_to admin
+  	  else
+      	redirect_to login_url
+ 	  	end
+		else
+			if session[:position] == ['Developer']
+				if developer = Developer.authenticate(params[:name], params[:password])
+  	    	session[:developer_id] = developer.id
+ 	    		redirect_to developer
+  	 		else
+      		redirect_to login_url
+ 	  		end
+			else
+				redirect_to login_url
+			end
+		end
   end
 
   def destroy
     session[:admin_id] = nil
+    session[:developer_id] = nil
     redirect_to login_url
   end
 end
--- app/models/developer.rb
+++ app/models/developer.rb
@@ -5,4 +5,13 @@ class Developer < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true
   validates :project, :presence => true
+  class << self
+    def authenticate(name, password)
+      if developer = find_by_name(name)
+        if developer.password == password
+          developer
+        end
+      end
+    end
+  end
 end
