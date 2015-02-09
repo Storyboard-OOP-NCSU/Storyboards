1f19335dca9708c038cdb9449033036f12f813f1
Winbobob <zhu6@ncsu.edu>
2015-02-09 00:30:44 -0500

    add UI framework

--- Gemfile
+++ Gemfile
@@ -39,3 +39,7 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+# Use Gumby Front End Framework
+gem 'gumby-framework'
+gem "modernizr-rails"
+gem "gumby-framework"
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require modernizr
+//= require gumby
+//= require ui/gumby.fittext
+//= require ui/gumby.navbar
+//= require ui/gumby.checkbox
+//= require ui/gumby.fixed
+//= require ui/gumby.radiobtn
+//= require ui/gumby.retina
+//= require ui/gumby.skiplink
+//= require ui/gumby.tabs
+//= require ui/gumby.toggleswitch
+//= require ui/jquery.validation
 //= require_tree .
