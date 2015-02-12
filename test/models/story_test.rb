aa1ee4c25132362674965fce0799a1af07624447
yshang3 <yshang3@ncsu.edu>
2015-02-12 14:49:19 -0500

    add one unit test example

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "story should have title, description, stage and point value" do
+    story = Story.new
+    assert_not story.save
+  end
+
+  test "story with title, description, stage, point value, project and line_item should be saved" do
+    story = Story.new
+    story.title = "some title"
+    story.description = "Some description"
+    story.stages = "Some stage"
+    story.point = "Some point value"
+    story.project = projects(:one)
+    story.line_items = line_items
+    assert story.save
+  end
+
 end
